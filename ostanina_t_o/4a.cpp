977722d79184ed8dc66f900a44a005ff9d97772b
OstaninaTatiana <65912322+OstaninaTatiana@users.noreply.github.com>
2022-10-10 21:27:13 +0300

    Add files via upload

---
+++ ostanina_t_o/4a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+ 
+//using namespace std;
+ 
+int main()
+{
+    int w=0;
+    std::cin >> w;
+    if ((w % 2 == 0) && (w > 2))
+    {
+        std::cout << "YES";
+    }
+    else
+    {
+        std::cout << "NO";
+    }
+    return 0;
+}
